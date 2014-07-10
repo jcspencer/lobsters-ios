0eabebf73d5de72e9a4340e27e9c4aeab949291e
James Spencer <jamess237@icloud.com>
2014-07-10 19:31:48 -0400

    Much more inline documentation and spacing

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,29 +1,38 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    # init window with frame of screen bounds
     @win = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
+    # configure status bar
     UIApplication.sharedApplication.setStatusBarStyle(UIStatusBarStyleLightContent)
     UIApplication.sharedApplication.setStatusBarHidden(false)
 
+    # create uinavigationcontroller witha custom gradient nav bar class
     vc = UINavigationController.alloc.initWithNavigationBarClass(CRGradientNavigationBar, toolbarClass: nil)
 
-    CRGradientNavigationBar.appearance.setBarTintGradientColors(['#FF5E3A'.to_color, '#FF2A68'.to_color])
-    #CRGradientNavigationBar.appearance.setBarTintGradientColors(['#55EFCB'.to_color, '#5BCAFF'.to_color])
+    # create a gradient nav bar
+    CRGradientNavigationBar.appearance.setBarTintGradientColors(['#FF5E3A'.to_color, '#FF2A68'.to_color]) # pink -> orange
+    #CRGradientNavigationBar.appearance.setBarTintGradientColors(['#55EFCB'.to_color, '#5BCAFF'.to_color]) # dark blue -> light blue
     vc.navigationBar.setTranslucent(false)
     vc.navigationBar.titleTextAttributes = {UITextAttributeTextColor => UIColor.whiteColor}
 
+    # init story view controller
     tv = StoryListController.alloc.initWithStyle(UITableViewStylePlain)
     vc.setViewControllers([tv])
 
+    # tints
     @win.tintColor = UIColor.whiteColor
     @win.backgroundColor = UIColor.whiteColor
 
+    # push root view controller
     @win.rootViewController = vc
     @win.rootViewController.wantsFullScreenLayout = true
     @win.makeKeyAndVisible
 
+    # enable system network activity indicator
     AFNetworkActivityIndicatorManager.sharedManager.setEnabled(true)
 
+    # remove back button labels
     UIBarButtonItem.appearance.setBackButtonTitlePositionAdjustment(UIOffsetMake(-1000, -1000), forBarMetrics:UIBarMetricsDefault)
 
     true
--- app/model/story.rb
+++ app/model/story.rb
@@ -3,6 +3,7 @@ class LobstersStory
   attr_accessor :comments, :score, :tags
   attr_accessor :date, :sid
 
+  # parsed json > variables
   def initialize(data)
     @url = NSURL.URLWithString(data["url"])
     @title = data["title"]
@@ -15,9 +16,14 @@ class LobstersStory
   end
 
   def generate_description
+    # get a relative date from now
     m = YLMoment.momentWithDate @date
     rel = m.fromNow
+
+    # pick arrow (up, down, stale)
     arrow = pick_arrow @score
+
+    # correct grammar for comment(s)
     use_s = ""
     if !@comments.eql? 1
       use_s = "s"
@@ -26,22 +32,24 @@ class LobstersStory
     "%s by %s\n%s %s | %d comment%s\n(%s)" % [rel, @author.username, arrow, @score, @comments, use_s, @url.host]
   end
 
-  def pick_arrow(score)
-    arrows = ["▲", "▼", "▣"]
-    if score == 0
-      arrows[2]
-    elsif score > 0
-      arrows[0]
-    elsif score < 0
-      arrows[1]
-    end
-  end
-
   private
-  def rfc_parse(iso_date)
+  # parse lobsters date (rfc)
+  def rfc_parse(rdate)
     formatter = NSDateFormatter.new
     formatter.setLocale(NSLocale.alloc.initWithLocaleIdentifier("en_US_POSIX"))
     formatter.setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZZZ")
-    formatter.dateFromString(iso_date)
+    formatter.dateFromString(rdate)
+  end
+
+  # returns an arrow depending on score (up, down, stale)
+  def pick_arrow(score)
+    arrows = ["▲", "▼", "▣"]
+    if score == 1 # score of 1 (default)
+      arrows[2]
+    elsif score > 1 # scores >1 (more than default)
+      arrows[0]
+    elsif score < 1 # scores 1< (less than default)
+      arrows[1]
+    end
   end
 end
